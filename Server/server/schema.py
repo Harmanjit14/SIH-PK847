90e089eac8de6ad710633b743013b15bcbd3a5b2
Harmanjit Singh <harmanjit140500@gmail.com>
2022-08-01 17:12:38 +0530

    base-auth

---
+++ Server/server/schema.py
@@ -0,0 +1,16 @@
+import graphene
+import graphql_jwt
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hi!")
+    pass
+
+class Mutation(graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+    pass
+
+
+schema = graphene.Schema(query=Query,mutation=Mutation)
