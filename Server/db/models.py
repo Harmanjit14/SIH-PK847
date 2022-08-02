aac12ea1dcc394bbbae050210198a8e1fe662f02
Harman <harmanjit140500@gmail.com>
2022-08-02 16:58:56 +0530

    student table

--- Server/db/models.py
+++ Server/db/models.py
@@ -1,12 +1,46 @@
+
+from django.core.validators import MinValueValidator
 from uuid import uuid4
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
 class Institute(models.Model):
-    id = models.UUIDField(default=uuid4,editable=False,primary_key=True)
-    name = models.CharField(blank=False,null=True,editable=True,max_length=255)
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    name = models.CharField(blank=False, null=True,
+                            editable=True, max_length=255)
     mail = models.EmailField(max_length=255)
+    logo = models.URLField(
+        default='https://firebasestorage.googleapis.com/v0/b/stately-pulsar-343510.appspot.com/o/static%2Fdeflogo.png?alt=media&token=d99783e2-c80c-4e4f-9300-f85f6aacac50')
+    signature = models.URLField(
+        default='https://firebasestorage.googleapis.com/v0/b/stately-pulsar-343510.appspot.com/o/static%2Fdefsignature.png?alt=media&token=a53e5c47-51e3-46cd-b415-4ca6e053e969')
+    contact = PhoneNumberField()
 
     def __str__(self):
         return self.name
+
+class Student(models.Model):
+
+    degree_choices = {
+        'BE':'Bachelors in Engineering',
+        'B.Tech':'Bachelors of Technology',
+        'BSc': 'Bachelors of Science'
+    }
+
+    id = models.UUIDField(default=uuid4,primary_key=True,editable=False)
+    roll = models.IntegerField()
+    name= models.CharField(max_length=255, blank=False, null=False)
+    email = models.EmailField(unique=True)
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE)
+    mobile =  PhoneNumberField()
+    dob = models.DateField()
+    graduating_year = models.IntegerField(max_length=4)
+    degree =  models.CharField(choices=degree_choices, max_length=255, default='BE')
+    address = models.CharField(max_length=255)
+    wallet = models.IntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f'{self.institute.name} {self.roll}'
     
--- Server/db/schema.py
+++ Server/db/schema.py
@@ -1,51 +1,51 @@
-import graphene
-from django.contrib.auth import get_user_model
-from graphene_django import DjangoObjectType
-from .models import *
-from graphql import GraphQLError
-from django.db.models import Q
+# import graphene
+# from django.contrib.auth import get_user_model
+# from graphene_django import DjangoObjectType
+# from .models import *
+# from graphql import GraphQLError
+# from django.db.models import Q
 
 
-class UserType(DjangoObjectType):
-    class Meta:
-        model = get_user_model()
+# class UserType(DjangoObjectType):
+#     class Meta:
+#         model = get_user_model()
 
 
-class CreateUser(graphene.Mutation):
-    user = graphene.Field(UserType)
+# class CreateUser(graphene.Mutation):
+#     user = graphene.Field(UserType)
 
-    class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        email = graphene.String(required=True)
+#     class Arguments:
+#         username = graphene.String(required=True)
+#         password = graphene.String(required=True)
+#         email = graphene.String(required=True)
 
-    def mutate(self, info, username, password, email, **kwargs):
-        user = get_user_model()(
-            username=username,
-            email=email,
-        )
-        user.set_password(password)
-        user.save()
+#     def mutate(self, info, username, password, email, **kwargs):
+#         user = get_user_model()(
+#             username=username,
+#             email=email,
+#         )
+#         user.set_password(password)
+#         user.save()
 
-        return CreateUser(user=user)
-
-
-
-class DeleteUser(graphene.Mutation):
-    user = graphene.String()
-
-    def mutate(self, info):
-        user = info.context.user
-
-        if user.is_anonymous:
-            raise GraphQLError("Not Logged In!")
-
-        user.delete()
-        str = "Done!"
-
-        return DeleteUser(user=str)
+#         return CreateUser(user=user)
 
 
-class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-    delete_user = DeleteUser.Field()
+
+# class DeleteUser(graphene.Mutation):
+#     user = graphene.String()
+
+#     def mutate(self, info):
+#         user = info.context.user
+
+#         if user.is_anonymous:
+#             raise GraphQLError("Not Logged In!")
+
+#         user.delete()
+#         str = "Done!"
+
+#         return DeleteUser(user=str)
+
+
+# class Mutation(graphene.ObjectType):
+#     create_user = CreateUser.Field()
+#     delete_user = DeleteUser.Field()
