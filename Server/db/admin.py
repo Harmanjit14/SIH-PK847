bd0573922657c7c62347dac9c39dc255003a5466
Harman <harmanjit140500@gmail.com>
2022-08-02 17:10:59 +0530

    admin update

--- Server/db/admin.py
+++ Server/db/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Student)
+admin.site.register(Academic_Records)
+admin.site.register(Institute)
 # Register your models here.
