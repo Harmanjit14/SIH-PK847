dcd9a46990d21bcfa3aff28a0ad1e3d186d6c0ff
Harman <harmanjit140500@gmail.com>
2022-08-05 02:44:45 +0530

    mailing system working

--- Server/server/settings.py
+++ Server/server/settings.py
@@ -176,4 +176,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'fridaynight.SIH@gmail.com'
-EMAIL_HOST_PASSWORD = 'Temp@12345'
+EMAIL_HOST_PASSWORD = 'lyuwfetgclawptoi'
