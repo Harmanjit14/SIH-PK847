fbc25d02bea49181b4697ac2f3d0627427964083
Harman <harmanjit140500@gmail.com>
2022-08-08 00:31:42 +0530

    db changes

--- Server/db/models.py
+++ Server/db/models.py
@@ -40,6 +40,8 @@ class Student(models.Model):
     wallet = models.IntegerField(validators=[MinValueValidator(0)])
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     batch = models.CharField(blank=True, max_length=255)
+    father_name = models.CharField(blank=True, max_length=255)
+    mother_name = models.CharField(blank=True, max_length=255)
 
     def __str__(self):
         return f'{self.institute.name} {self.roll}'
@@ -50,13 +52,13 @@ class Academic_Record(models.Model):
                           editable=False, primary_key=True)
     semester = models.IntegerField()
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    subject_code = models.CharField(max_length=255,blank=False, null=False)
+    subject_code = models.CharField(max_length=255, blank=False, null=False)
     subject = models.CharField(
         blank=False, null=False, editable=True, max_length=255)
     grade = models.CharField(blank=False, null=False,
                              editable=True, max_length=2)
     marks = models.IntegerField(null=True, validators=[MinValueValidator(0)])
-    credits = models.FloatField(default=0,null=False, blank=False)
+    credits = models.FloatField(default=0, null=False, blank=False)
 
     def __str__(self):
         return f'{self.id} {self.student.institute.name}'
