2d6bf7dcbe4f3ddfa150c125983c68a5f03f42cf
unknown <Manroop>
2022-08-12 19:00:53 +0530

    Backend Destroyed

--- NLP/queries.py
+++ NLP/queries.py
@@ -56,6 +56,114 @@ def get_migration_certificate(client):
         return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
 
 
+def get_character_certificate(client):
+
+    query = """
+    {
+        characterCertificate
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['characterCertificate'] == 'Success':
+            return "ਮੈਂ ਤੁਹਾਡਾ ਅੱਖਰ ਸਰਟੀਫਿਕੇਟ ਬਣਾਇਆ ਹੈ ਅਤੇ ਤੁਹਾਨੂੰ ਡਾਕ ਰਾਹੀਂ ਭੇਜ ਦਿੱਤਾ ਹੈ। ਕ੍ਰਿਪਾ ਜਾਂਚ ਕਰੋ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਡਾ ਅੱਖਰ ਸਰਟੀਫਿਕੇਟ ਬਣਾਉਣ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
+def get_semester_certificate(client):
+
+    query = """
+    {
+        semesterCertificate
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['semesterCertificate'] == 'Success':
+            return "ਮੈਂ ਤੁਹਾਡਾ ਸਮੈਸਟਰ ਸਰਟੀਫਿਕੇਟ ਬਣਾਇਆ ਹੈ ਅਤੇ ਤੁਹਾਨੂੰ ਡਾਕ ਰਾਹੀਂ ਭੇਜ ਦਿੱਤਾ ਹੈ। ਕ੍ਰਿਪਾ ਜਾਂਚ ਕਰੋ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਡਾ ਸਮੈਸਟਰ ਸਰਟੀਫਿਕੇਟ ਬਣਾਉਣ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
+def get_domicile_certificate(client):
+
+    query = """
+    {
+        domicileCertificate
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['domicileCertificate'] == 'Success':
+            return "ਮੈਂ ਤੁਹਾਡਾ ਨਿਵਾਸ ਸਰਟੀਫਿਕੇਟ ਬਣਾਇਆ ਹੈ ਅਤੇ ਤੁਹਾਨੂੰ ਡਾਕ ਰਾਹੀਂ ਭੇਜ ਦਿੱਤਾ ਹੈ। ਕ੍ਰਿਪਾ ਜਾਂਚ ਕਰੋ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਡਾ ਨਿਵਾਸ ਸਰਟੀਫਿਕੇਟ ਬਣਾਉਣ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
+def get_affidavit(client):
+
+    query = """
+    {
+        affidavit
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['affidavit'] == 'Success':
+            return "ਮੈਂ ਤੁਹਾਡਾ ਹਲਫੀਆ ਬਿਆਨ ਬਣਾਇਆ ਹੈ ਅਤੇ ਤੁਹਾਨੂੰ ਡਾਕ ਰਾਹੀਂ ਭੇਜ ਦਿੱਤਾ ਹੈ। ਕ੍ਰਿਪਾ ਜਾਂਚ ਕਰੋ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਡਾ ਹਲਫੀਆ ਬਿਆਨ ਬਣਾਉਣ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
+def get_institute_info(client):
+
+    query = """
+    {
+        instituteInformation
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['instituteInformation'] == 'Success':
+            return "ਮੈਂ ਸੰਸਥਾ ਦੀ ਉਹ ਜਾਣਕਾਰੀ ਦੇ ਦਿੱਤੀ ਹੈ ਜੋ ਤੁਸੀਂ ਮੰਗੀ ਸੀ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਪਰ ਮੈਂ ਤੁਹਾਨੂੰ ਸੰਸਥਾ ਦੀ ਜਾਣਕਾਰੀ ਨਹੀਂ ਦੇ ਸਕਦਾ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
+def get_institute_events_info(client):
+
+    query = """
+    {
+        instituteEventsInformation
+    }
+    """
+    try:
+        result = client.execute(query=query)
+        if result['data']['instituteEventsInformation'] == 'Success':
+            return "ਮੈਂ ਸੰਸਥਾ ਦੇ ਸਮਾਗਮਾਂ ਬਾਰੇ ਜਾਣਕਾਰੀ ਦੇ ਦਿੱਤੀ ਹੈ ਜੋ ਤੁਸੀਂ ਮੰਗੀ ਸੀ"
+        else:
+            return "ਮਾਫ਼ ਕਰਨਾ ਪਰ ਮੈਂ ਤੁਹਾਨੂੰ ਇੰਸਟੀਚਿਊਟ ਦੇ ਸਮਾਗਮਾਂ ਬਾਰੇ ਜਾਣਕਾਰੀ ਨਹੀਂ ਦੇ ਸਕਦਾ"
+
+    except:
+        return "ਮਾਫ਼ ਕਰਨਾ ਮੈਂ ਤੁਹਾਨੂੰ ਲੌਗਇਨ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਸੀ"
+
+
 def case_selector(id, obj):
 
     # Server Connection
@@ -73,3 +181,16 @@ def case_selector(id, obj):
         return False
     if id == 1:
         return get_migration_certificate(server_client)
+    elif id == 2:
+        return get_character_certificate(server_client)
+    elif id == 3:
+        return get_semester_certificate(server_client)
+    elif id == 4:
+        return get_domicile_certificate(server_client)
+    elif id == 5:
+        return get_affidavit(server_client)
+    elif id == 6:
+        return get_institute_info(server_client)
+    elif id == 7:
+        return get_institute_events_info(server_client)
+
